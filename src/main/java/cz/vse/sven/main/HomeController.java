785ed07043f868b6ca22566ff67f555817f82225
Tomáš Kotouč <kott10@vse.cz>
2024-02-16 19:56:23 +0100

    napojení na rozhraní logických tříd a přidání dialogu k ukončení hry, hra je nyní hratelná v grafické verzi

--- src/main/java/cz/vse/sven/main/HomeController.java
+++ src/main/java/cz/vse/sven/main/HomeController.java
@@ -1,9 +1,13 @@
 package cz.vse.sven.main;
 
+import cz.vse.sven.logika.hra.Hra;
+import cz.vse.sven.logika.hra.IHra;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+
+import java.util.Optional;
 
 /**
  * Controller pro home.fxml
@@ -11,19 +15,58 @@ import javafx.scene.control.TextField;
 public class HomeController {
 
     @FXML
+    private Button tlacitkoOdesli;
+
+    @FXML
     private TextArea vystup;
 
     @FXML
     private TextField vstup;
 
+    private IHra hra = new Hra();
+
+    /**
+     * Metoda na začátku hry: vrací uvítání a focusuje na TextField
+     */
+    @FXML
+    private void initialize() {
+        vystup.appendText(hra.vratUvitani());
+        Platform.runLater(() -> vstup.requestFocus());
+    }
+
     /**
      * Metoda vypisuje vstup (zadaný v TextField) do výstupu (TextArea)
+     * dále daný vstup zpracuje a vypíše jeho výsledek
+     *
+     * pokud nastane konec hry, tak vypíše epilog a zamezí interakci se vstupem(TextField) a tlačítkem "Odešli"
      *
      * @param actionEvent
      */
     @FXML
     private void odesliVstup(ActionEvent actionEvent) {
-        vystup.appendText(vstup.getText() + "\n");
+        String prikaz = vstup.getText();
+        vystup.appendText("> " + prikaz + "\n");
+        String vysledek = hra.zpracujPrikaz(prikaz);
+        vystup.appendText(vysledek + "\n");
         vstup.clear();
+
+        if (hra.konecHry()) {
+            vystup.appendText(hra.vratEpilog());
+            vstup.setDisable(true);
+            tlacitkoOdesli.setDisable(true);
+        }
+    }
+
+    /**
+     * Metoda zajistí, že po kliknutí na položku "Ukončit" a následným potvrzením "OK" se hra zavře
+     *
+     * @param actionEvent
+     */
+    public void ukoncitHru(ActionEvent actionEvent) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Opravdu chtete ukončit hru? Veškerý postup bude ztracen.");
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.isPresent() && result.get() == ButtonType.OK) {
+            Platform.exit();
+        }
     }
 }
